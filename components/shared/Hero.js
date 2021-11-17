a4ba2864b455ac5edaef77d8a220d5866c4e7bcd
Jannick <jannickpedersen@live.dk>
2021-11-17 19:08:19 +0100

    edit hero.js

--- components/shared/Hero.js
+++ components/shared/Hero.js
@@ -3,10 +3,10 @@ const Hero = () =>
         <section className="fj-hero">
         <div className="fj-hero-wrapper row">
         <div className="hero-left col-md-6">
-            <h1 className="white hero-title">Hey I'm Filip. Experienced full stack developer</h1>
-            <h2 className="white hero-subtitle">Check my portfolio and video tutorials</h2>
+            <h1 className="white hero-title">Hey I'm Jannick Pedersen. Experienced full stack developer</h1>
+            <h2 className="white hero-subtitle">This is my NextJS udemy course made by Filip Jarger</h2>
             <div className="button-container">
-            <a href="" className="btn btn-main bg-blue ttu">See my work</a>
+            <a href="https://github.com/JannickPepe" className="btn btn-main bg-blue ttu">See my work GIT repo</a>
             </div>
         </div>
         <div className="hero-right col-md-6">
