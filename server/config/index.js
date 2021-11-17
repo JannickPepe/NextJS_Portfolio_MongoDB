57b6d48905d2eeb40880164a064c7d755baf4579
Jannick <jannickpedersen@live.dk>
2021-11-17 15:25:08 +0100

    prod session setup

--- server/config/index.js
+++ server/config/index.js
@@ -6,4 +6,3 @@ if (process.env.NODE_ENV === 'production') {
         module.exports = require('./dev');
 }
 
-//mongodb+srv://jannickportfolio:Rasengan1991@cluster0.eqe2n.mongodb.net/portfolioDb?retryWrites=true&w=majority
