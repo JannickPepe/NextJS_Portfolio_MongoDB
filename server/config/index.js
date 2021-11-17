3ac771a8bea87c294aed24cbdccbe31eb565d26b
Jannick <jannickpedersen@live.dk>
2021-11-17 15:17:12 +0100

    prod database changes

--- server/config/index.js
+++ server/config/index.js
@@ -5,3 +5,5 @@ if (process.env.NODE_ENV === 'production') {
 } else {
         module.exports = require('./dev');
 }
+
+//mongodb+srv://jannickportfolio:Rasengan1991@cluster0.eqe2n.mongodb.net/portfolioDb?retryWrites=true&w=majority
