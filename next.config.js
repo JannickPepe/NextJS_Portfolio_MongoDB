742ca9e27fc7feee9ddcb7abcbc99e578cd1ebc1
Jannick <jannickpedersen@live.dk>
2021-11-17 14:58:22 +0100

    baseUrl

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
+
+
 const path = require('path');
+
+const dev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   webpack: config => {
 
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
+  },
+  env: {
+    BASE_URL: dev ? 'http://localhost:3000/graphql' : 'https://jannick-portfolio.herokuapp.com/graphql'
   }
 }
