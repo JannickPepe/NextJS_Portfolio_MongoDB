90af824606605650ea0a20f8300aa1866e26f55f
Jannick <jannickpedersen@live.dk>
2021-11-17 14:47:31 +0100

    CV added

--- pages/cv.js
+++ pages/cv.js
@@ -1,3 +1,13 @@
+import BaseLayout from '@/layouts/BaseLayout';
 
-const Cv = () => <h1>Hello CV Page</h1>
+const Cv = () =>
+    <BaseLayout>
+        <div className="row mt-4">
+        <div className="col-md-8 offset-md-2">
+            <iframe src="/CV_FrontendDev_Candeno.pdf" style={{width: '100%', height: '800px'}}>
+            </iframe>
+        </div>
+        </div>
+    </BaseLayout>
+
 export default Cv;
